68f4f9dba550501d6e01780d12ec1907d08b3f5e
Sam Meech-Ward <sam@meech-ward.com>
2021-09-12 21:44:02 -0700

    Add week 2 tests

--- app.js
+++ app.js
@@ -32,4 +32,62 @@ app.get('/stats', async (req, res) => {
     res.send(data)
 })
 
+async function ec2Details() {
+  let ec2 = {};
+  ec2.ipv4 = await ec2Meta.ipv4();
+  ec2.hostname = await ec2Meta.hostname();
+  ec2.instanceId = await ec2Meta.instanceId();
+  return ec2;
+}
+
+const nginxIP = process.env.NGINX_IP
+const mysqlIP = process.env.MYSQL_IP
+
+async function nginxTest() {
+  const result = await axios.get(`http://${nginxIP}`);
+  if (result.status >= 200 || result.status < 300) {
+    return nginxIP;
+  }
+  throw `Nginx server setup incorrectly at ip: ${nginxIP}`;
+}
+
+const mysql = require("mysql2/promise");
+
+async function mysqlTest() {
+  const connection = await mysql.createConnection({
+    host: mysqlIP,
+    user: "MyNewUser",
+    password: "MyNewPass4!",
+  });
+
+  const [rows, fields] = await connection.execute(
+    "SELECT NOW()"
+  );
+
+  return {
+    ip: mysqlIP,
+    data: rows[0]
+  }
+}
+
+app.get("/grade", async (req, res) => {
+  try {
+    if (!mysqlIP) {
+      throw "You need to provide a MYSQL_IP";
+    }
+    if (!nginxIP) {
+      throw "You need to provide a NGINX_IP";
+    }
+    let data = {}
+    data.ec2Details = ec2Details();
+    data.nginx = nginxTest();
+    data.mysql = mysqlTest();
+    console.log(data);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
 app.listen(3640, () => console.log("listening on port 3640"))
